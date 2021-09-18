d3899712fd81b5d22e7f83e58bc5fb2d5ed84f5b
jef <jeferson.s.silva1@gmail.com>
2021-09-18 17:26:09 -0300

    finish project

---
+++ script.js
@@ -0,0 +1,90 @@
+let validator = {
+  handleSubmit: event => {
+    event.preventDefault()
+    
+    let send = true
+
+    let inputs = form.querySelectorAll('input');
+
+    validator.clearErrors();
+
+    for(let i = 0; i < inputs.length; i++){
+      let input = inputs[i];
+
+      let check = validator.checkInput(input)
+
+      if(check !== true){
+        send = false;
+        validator.showError(input, check);
+      }
+    }
+
+    send ? form.submit() : ''
+
+  },
+
+  checkInput: input => {
+    let rules = input.getAttribute('data-rules')
+
+    if(rules !== null){
+      rules = rules.split('|');
+
+      for(let k in rules) {
+        let ruleDetails = rules[k].split('=');
+
+        switch(ruleDetails[0]) {
+          case 'required':
+            if(input.value == '') {
+              return 'Campo não pode ser vazio.'
+            }
+          break;
+          
+          case 'min':
+            if(input.value.length < ruleDetails[1]) {
+              return 'Campo deve ter no mínimo ' + ruleDetails[1] + ' caracteres.'
+            }
+          break;
+          
+          case 'email':
+            if(input.value != '') {
+              let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+              if(!regex.test(input.value.toLowerCase())) {
+                return 'E-mail não é válido!';
+              }
+            }
+          break;
+        }
+      }
+    }
+
+    return true;
+  },
+
+  showError: (input, error) => {
+    input.style.borderColor = 'red';
+
+    let errorElement = document.createElement('div');
+    errorElement.classList.add('error');
+    errorElement.innerHTML = error;
+
+    input. parentElement.insertBefore(errorElement, input.ElementSibling);
+  },
+
+  clearErrors: () => {
+    let inputs = form.querySelectorAll('input');
+    for(let i = 0; i < inputs.length; i++){
+      inputs[i].style = '';
+    }
+
+
+    let errorElement = document.querySelectorAll('.error');
+    for(let i = 0; i < errorElement.length; i++){
+      errorElement[i].remove();
+    }
+  }
+};
+
+let form = document.querySelector('.validator');
+
+form.addEventListener('submit', validator.handleSubmit);
